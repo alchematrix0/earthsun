f923da3abcfd95660f4c5e8533111083d052a30e
desforets <md5@openmailbox.org>
2018-05-17 09:18:09 -0700

    live keys

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,7 @@
 // Store API
-// var serverURL = 'https://www.earthsun.ca'
+var serverURL = 'https://www.earthsun.ca'
 var stripe = Stripe('pk_live_wQ8l7gZKVSvCfc5P6E0Qq2Lq')
-var serverURL = 'http://localhost:3000'
+// var serverURL = 'http://localhost:3000'
 // var stripe = Stripe('pk_test_u77KpSLxrO1jKMrKyA9CZWhy');
 
 let existingCustomer = null
